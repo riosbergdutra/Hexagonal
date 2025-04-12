85761ceed3ed20ca75b36e8478089f483f03fa70
Riosberg Dutra <riosbergduttra@gmail.com>
2025-04-12 17:26:20 -0300

    delets

--- hexagonal/src/main/java/com/example/hexagonal/adapters/output/repository/JpaUsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/example/hexagonal/adapters/output/repository/JpaUsuarioRepository.java
-package com.example.hexagonal.adapters.output.repository;
-
-import com.example.hexagonal.adapters.output.entities.JpaUsuarioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaUsuarioRepository extends JpaRepository<JpaUsuarioEntity, Long> {
-}
--- hexagonal/src/main/java/com/example/hexagonal/domain/port/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hexagonal.domain.port;
-
-import com.example.hexagonal.domain.Usuario;
-import java.util.Optional;
-
-public interface UsuarioRepository {
-    Usuario save(Usuario usuario);
-    Optional<Usuario> findById(Long id);
-}
